93dae60694d9ba24733a01426587109a9f6dbaa6
Eric.Lin <koty310055@gmail.com>
2021-05-04 04:06:52 +0800

    add vuex example

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    userInfo: {
+      name: "Eric",
+      email: "koty310055@gmail.com",
+      age: 23,
+    },
+  },
+  mutations: {
+    addAge(state, payload) {
+      state.userInfo.age = payload.count;
+    },
+  },
   actions: {},
   modules: {},
 });
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,5 +1,12 @@
 <template>
   <keep-alive>
+    <v-card class="mx-auto" max-width="344">
+      <v-card-text>
+        <div>{{ $store.state.userInfo }}</div>
+      </v-card-text>
+      <v-btn @click="testcommit(50)" color="primary">年齡</v-btn>
+    </v-card>
+
     <article-list v-if="isShow" @switch-article="switchArticle"></article-list>
     <article-page v-else @switch-article="switchArticle"></article-page>
   </keep-alive>
@@ -21,6 +28,12 @@ export default {
   methods: {
     switchArticle() {
       this.isShow = !this.isShow;
+    },
+    testcommit(count) {
+      this.$store.commit({
+        type: "addAge",
+        count,
+      });
     },
   },
 };
